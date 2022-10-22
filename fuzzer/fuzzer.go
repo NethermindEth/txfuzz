82cfa7d51b7e2d7b70887a26798818f014d62def
Jorge Mederos <jmederosalvarado@gmail.com>
2022-10-22 14:41:32 -0400

    Airdrop

--- fuzzer/fuzzer.go
+++ fuzzer/fuzzer.go
@@ -156,7 +156,8 @@ func (fuzzer *TxFuzzer) doAirdrop(addrs []common.Address) {
 		lastTx = signedTx
 	}
 	if lastTx == nil {
-		logger.Default().Fatalf("Airdrop failed")
+		logger.Default().Printf("Airdrop finished without airdropping anything")
+        return
 	}
 	// Wait for the last transaction to be mined
 	bind.WaitMined(context.Background(), fuzzer.client, lastTx)
